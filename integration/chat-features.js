5371a39e22039421e2fcd90355faf0b17d999117
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-01-28 20:20:04 +0000

    update(cypress): minor update

--- integration/chat-features.js
+++ integration/chat-features.js
@@ -6,26 +6,19 @@ const textToPaste = 'copy paste stuff'
 
 it('Chat - Send stuff on chat', () => {
   cy.importAccount()
-  cy.contains('aaaaa').click() //clicks on user name
+  cy.contains('aaaaa').click() // clicks on user name
   cy.get('.messageuser').type(randomMessage)
-  cy.get('.messageuser').type('{enter}') //to send out a written message
+  cy.get('.messageuser').type('{enter}') // sending text message
   cy.contains(randomMessage)
   cy.get('#emoji-toggle > .control-icon').click()
-  cy.get('[title="smile"]').click() //to send out a emoji
+  cy.get('[title="smile"]').click() // sending emoji
   cy.get('.messageuser').click()
   cy.get('.messageuser').type('{enter}')
   cy.contains('😄')
   cy.contains(randomMessage).rightclick()
   cy.contains('Edit Message').click()
   cy.get('.edit-message-body-input > p').click()
-  cy.get('.edit-message-body-input > p').type(randomNumber)
+  cy.get('.edit-message-body-input > p').type(randomNumber) // editing message
   cy.get('.edit-message-body-input > p').type('{enter}')
   cy.contains(randomNumber)
-  /*
-  cy.contains('Reply').click() //to reply on a thread - to be added when AP-271 is fixed
-  cy.contains('Reply to')
-  cy.get('.messageuser').type(randomWord)
-  cy.get('.messageuser').type('{enter}')
-  */
-  // add copy paste test
 })
