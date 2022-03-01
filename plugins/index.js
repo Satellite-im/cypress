4a7c87c9f83c57a375fec64104c1bac908b16f69
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-03-01 00:50:22 +0000

    chore(config): revert

--- plugins/index.js
+++ plugins/index.js
@@ -1,12 +1,6 @@
 const { initPlugin } = require('cypress-plugin-snapshots/plugin')
 
 module.exports = (on, config) => {
-  on('before:browser:launch', (browser, launchOptions) => {
-    if (browser.name === 'chrome' && browser.isHeadless) {
-      launchOptions.args.push('--disable-gpu')
-      return launchOptions
-    }
-  })
   initPlugin(on, config)
   return config
 }
